aef92f567af8aa5fe605c3c93ae2505c7ee43540
202204156 <144645205+202204156@users.noreply.github.com>
2023-11-20 13:24:18 +0900

    Add files via upload

---
+++ 과제 #2/GraphicEditor.h
@@ -0,0 +1,16 @@
+#ifndef GRAPHICEDITOR_H
+#define GRAPHICEDITOR_H
+#include "Shape.h"
+
+class GraphicEditor {
+	Shape* pStart;
+	Shape* pLast;
+public:
+	GraphicEditor() { pStart = pLast= NULL; }
+	void insertItem(int num);
+	void deleteItem(int num);
+	void show();
+	void run();
+};
+
+#endif
---
+++ 과제 #2/UI.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+#include <iostream>
+using namespace std;
+
+class UI {
+public:
+	static int getMenu();
+	static int getShapeTypeToInsert();
+	static int getShapeIndexToDelete();
+};
+
+#endif
